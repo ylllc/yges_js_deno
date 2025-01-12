099c4ca19649fd11e4ba3d2f6a8fb00c17c5305e
ylllc <leprechaun@yggdrasil-leaves.com>
2025-01-12 08:45:41 +0000

    split HTTP LowLevel

---
+++ api/http_ll.js
@@ -0,0 +1,36 @@
+﻿// † Yggdrasil Essense for JavaScript † //
+// ====================================== //
+// © 2024-5 Yggdrasil Leaves, LLC.        //
+//        All rights reserved.            //
+
+import YgEs from './common.js';
+
+import http from 'node:http';
+import path from 'node:path';
+import mime from 'npm:mime-lite';
+
+// HTTP Server Low Level for Deno-------- //
+(()=>{ // local namespace 
+
+function _setup(cb_req){
+
+	return http.createServer(cb_req);
+}
+
+function _getMIMEType(stat){
+
+	var ext=path.extname(stat.GetPath());
+	if(!ext)return null;
+	return mime.getType(ext.substring(1));
+}
+
+let HTTPLowLevel=YgEs.HTTPLowLevel={
+	name:'YgEs.HTTPLowLevel',
+	User:{},
+
+	SetUp:_setup,
+	GetMIMEType:_getMIMEType,
+}
+
+})();
+export default YgEs.HTTPLowLevel;
