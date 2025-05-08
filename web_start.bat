3b811e757f76c6e512f7de28d917a5c60adb0850
ylllc <leprechaun@yggdrasil-leaves.com>
2025-05-08 21:51:27 +0000

    add WebSocket example

--- web_start.bat
+++ web_start.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
 if exist _setpath.bat call _setpath.bat
 cd example
-deno run --allow-net --allow-read --allow-write 100-http_server.js
+deno run --allow-net --allow-read --allow-write --allow-env 101-http_server_websock.js
 pause
